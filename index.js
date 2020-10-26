ecbf72b86e2072156eb011f9dc8236b5597df07a
Jimmy Michalec <james.michalec58@gmial.com>
2020-10-26 17:20:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return object[key]=value;
+  object[key]=value;
+  return object;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
